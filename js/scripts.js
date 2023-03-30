4fc6a94afc48cd94d7e59cf8206a88c24e52afee
rglee2 <80657157+rglee2@users.noreply.github.com>
2023-03-30 15:10:22 -0400

    Update js
    added new variable bigPokemon for large pokemons.
    
    Added for loop to list pokemon name and height.
    If pokemon is big, will write out the new variable next to list.

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,15 @@ let pokemonList = [
      type: 'water',
   },
 ];
+
+//variable for big Pokemon
+let bigPokemon = "Wow, that's big!"
+
+//condition nested in for loop to write list of pokemon and add big Pokemon variable if true
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 1.5) {
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + bigPokemon + '<br>'); 
+  } else {
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + '<br>');
+  }
+}
