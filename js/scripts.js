16ad62c0a832b0f581f889abc3b3a660abbb2c86
rglee2 <80657157+rglee2@users.noreply.github.com>
2023-03-31 19:31:14 -0400

    Update scripts.js
    commented out previous if else loop, and added forEach condition to list Pokemon. Added spacing to pokemonList types for easier readability

--- js/scripts.js
+++ js/scripts.js
@@ -2,28 +2,35 @@ let pokemonList = [
   {
      name: 'bulbasaur',
      height: 0.7,
-     type: ['grass', 'poison'],
+     type: ['grass ', 'poison'],
   },
   {
      name: 'ivysaur',
      height: 1,
-     type: ['fire', 'flying'],
+     type: ['fire ', 'flying'],
   },
   {
      name: 'venusaur',
      height: 2,
-     type: 'water',
-  },
+    type: 'water ',
+},
 ];
 
 //variable for big Pokemon
 let bigPokemon = "Wow, that's big!"
 
 //condition nested in for loop to write list of pokemon and add big Pokemon variable if true
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 1.5) {
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + bigPokemon + '<br>'); 
-  } else {
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + '<br>');
-  }
+// for (let i = 0; i < pokemonList.length; i++) {
+// if (pokemonList[i].height > 1.5) {
+//   document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + bigPokemon + '<br>'); 
+// } else {
+//   document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' + '<br>');
+// }
+// }
+
+//forEach Condition to List Pokemon
+function pokemonInfo(pokemon) {
+  document.write('name: ' + pokemon.name + ' height: ' + pokemon.height + ' type: ' + pokemon.type + '<br>');
 }
+
+pokemonList.forEach(pokemonInfo);
