b8ba24d413d81c9f7a8fc102a096bb0a93397370
rglee2 <80657157+rglee2@users.noreply.github.com>
2023-03-31 19:46:09 -0400

    Update scripts.js
    put var pokemonList in IIFE with add and getAll functions. Updated forEach condition to reflect updated IIFE.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,40 @@
-let pokemonList = [
-  {
-     name: 'bulbasaur',
-     height: 0.7,
-     type: ['grass ', 'poison'],
-  },
-  {
-     name: 'ivysaur',
-     height: 1,
-     type: ['fire ', 'flying'],
-  },
-  {
-     name: 'venusaur',
-     height: 2,
-    type: 'water ',
-},
-];
+//IIFE for pokemonRepository with objects to add pokemon or List pokemon.
+let pokemonRepository = (function() {
+  let pokemonList = [
+    {
+       name: 'bulbasaur',
+       height: 0.7,
+       type: ['grass ', 'poison'],
+    },
+    {
+       name: 'ivysaur',
+       height: 1,
+       type: ['fire ', 'flying'],
+    },
+    {
+       name: 'venusaur',
+       height: 2,
+      type: 'water ',
+    },
+    ];
+  
+    function getAll() {
+      return pokemonList;
+    }
+
+    function add() {
+      pokemonList.push(pokemon);
+    }
+
+    return {
+      getAll: getAll,
+      add: add
+    }
+})();
+
 
 //variable for big Pokemon
-let bigPokemon = "Wow, that's big!"
+// let bigPokemon = "Wow, that's big!"
 
 //condition nested in for loop to write list of pokemon and add big Pokemon variable if true
 // for (let i = 0; i < pokemonList.length; i++) {
@@ -32,5 +49,5 @@ let bigPokemon = "Wow, that's big!"
 function pokemonInfo(pokemon) {
   document.write('name: ' + pokemon.name + ' height: ' + pokemon.height + ' type: ' + pokemon.type + '<br>');
 }
-
-pokemonList.forEach(pokemonInfo);
+//List Pokemon
+pokemonRepository.getAll().forEach(pokemonInfo);
